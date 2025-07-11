ca3c5d6b8b9d83255891277160df46a3a192106c
valify1 <theravens.pr@gmail.com>
2025-07-11 23:37:58 +0100

    Hunter

--- Hunter.lua
+++ Hunter.lua
@@ -176,9 +176,40 @@ F6 =  {
 		},
 
 	},
+
 	
-	-- add model draw args for network transmitting to this draw_args table (32 limit)
-	
+	net_animation = {
+        0, -- front gear
+		2,  -- nose wheel steering
+        3, -- right gear
+        5, -- left gear
+        9, -- right flap
+        10, -- left flap
+        11, -- right aileron
+        12, -- left aileron
+		13, -- right slat
+		14, -- left slat
+        15, -- right elevator
+        16, -- left elevator
+        17, -- rudder
+        21, -- SFM air brake
+        25, -- tail hook
+		28, -- nachbrenner
+		35, --	brakeshute
+		36, --	brakeshute
+		37, --	brakeshute
+        38, -- canopy
+		89, -- Triebwerk
+		182, -- air brake
+        190, -- left (red) navigation wing-tip light
+        191, -- right (green) navigation wing-tip light
+        192, -- tail (white) light
+        198, -- anticollision (flashing red) top light
+        199, -- anticollision (flashing red) bottom light
+    	208, -- taxi light (white) right main gear door
+		209, -- taxi light (white) right main gear door
+    },
+
 
 					
 	mapclasskey 		= "P0091000025",
@@ -212,13 +243,13 @@ F6 =  {
 	has_afteburner 				= false, -- AFB yes/no
 	has_speedbrake 				= true, -- Speedbrake yes/no
 	
-	nose_gear_pos 				                = {3.745, -1.705, 0},   -- position of gear
+	nose_gear_pos 				                = {3.745, -1.700, 0},   -- position of gear
 	    nose_gear_amortizer_direct_stroke   		=  0.000,  -- down from nose_gear_pos
 	    nose_gear_amortizer_reversal_stroke  		=  -0.160,  -- up 0.161m 3D Modell MK6
 	    nose_gear_amortizer_normal_weight_stroke 	=  -0.110,   -- up
 	    nose_gear_wheel_diameter 	                =  0.455, -- 3D Modell MK6
 
-	    main_gear_pos 						 	    = {-0.790, -1.730, 2.300}, -- changed back
+	    main_gear_pos 						 	    = {-0.790, -1.700, 2.300}, -- changed back
 	    main_gear_amortizer_direct_stroke	 	    =  0.000, -- down from main_gear_pos
 	    main_gear_amortizer_reversal_stroke  	    =  -0.240, -- up 0.24m 3D Modell MK6
 	    main_gear_amortizer_normal_weight_stroke    =  -0.170,-- down from main_gear_pos
